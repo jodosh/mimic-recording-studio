b503a70f6d6bcec318951f7298b280261b2a32eb
jodosh <jodosh@users.noreply.github.com>
2024-09-28 06:34:42 -0500

    working on updating docker

--- backend/start_prod.sh
+++ backend/start_prod.sh
@@ -1,1 +1,1 @@
-gunicorn -w $WEBWORKERS -b 0.0.0.0:$APIPORT app:app -c gunicorn_conf.py --capture-output
+gunicorn -w $WEBWORKERS -b 0.0.0.0:$APIPORT app:app -c gunicorn_conf.py --log-file gunicorn.log --access-logfile access.log --error-logfile error.log
